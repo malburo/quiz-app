ae11dd3cece7a09a3ceec0d4238931e34ab46799
malburo <tongquocbao0605@gmail.com>
2021-02-19 09:47:21 +0700

    Using prettier

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, View } from "react-native";
-import LoginScreen from "./features/auth/screens/Login";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import LoginScreen from './features/auth/screens/Login';
 
 export default function App() {
   return (
@@ -15,8 +15,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
--- components/form-control/InputField.jsx
+++ components/form-control/InputField.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { Controller } from "react-hook-form";
-import { TextInput } from "react-native-paper";
+import React from 'react';
+import { Controller } from 'react-hook-form';
+import { TextInput } from 'react-native-paper';
 
 const InputField = (props) => {
   const { form, disabled, name, label, placeholder } = props;
