c6cea69c94ed7489f41ec456c8e208ed30ce03bd
malburo <tongquocbao0605@gmail.com>
2021-02-20 14:47:35 +0700

    create PasswordField.jsx

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import LoginScreen from './features/auth/screens/Login';
+import RegisterScreen from './src/features/auth/screens/Register';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <LoginScreen />
+      <RegisterScreen />
       <StatusBar style="auto" />
     </View>
   );
