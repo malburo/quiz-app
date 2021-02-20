c63f6838bed6e88071ee2af097dbde5433d7ced6
malburo <tongquocbao0605@gmail.com>
2021-02-20 14:47:57 +0700

    create Register screen

---
+++ src/features/auth/screens/Register.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import RegisterForm from '../components/RegisterForm';
+
+const RegisterScreen = (props) => {
+  return <RegisterForm />;
+};
+
+export default RegisterScreen;
