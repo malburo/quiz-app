b4b3916007025fc653a5931ba4256b3829a5094e
malburo <tongquocbao0605@gmail.com>
2021-02-25 22:08:14 +0700

    Style InputField and PasswordField

--- src/components/form-control/InputField.jsx
+++ src/components/form-control/InputField.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Controller } from 'react-hook-form';
-import { TextInput } from 'react-native-paper';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 const InputField = (props) => {
   const { form, disabled, name, label, placeholder } = props;
@@ -14,15 +14,30 @@ const InputField = (props) => {
       disabled={disabled}
       placeholder={placeholder}
       render={(props) => (
-        <TextInput
-          {...props}
-          onChangeText={(value) => {
-            props.onChange(value);
-          }}
-        />
+        <View style={styles.container}>
+          <Text style={styles.text}>{label}</Text>
+          <TextInput
+            {...props}
+            style={styles.input}
+            onChangeText={(value) => {
+              props.onChange(value);
+            }}
+          />
+        </View>
       )}
     />
   );
 };
+
+const styles = StyleSheet.create({
+  container: { marginHorizontal: 24, marginVertical: 12, fontSize: 14, fontWeight: '500' },
+  text: { marginBottom: 12, marginLeft: 6, color: '#FFF' },
+  input: {
+    padding: 16,
+    color: '#FFF',
+    backgroundColor: '#1F1F30',
+    borderRadius: 18,
+  },
+});
 
 export default InputField;
--- src/components/form-control/PasswordField.jsx
+++ src/components/form-control/PasswordField.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Controller } from 'react-hook-form';
-import { TextInput } from 'react-native-paper';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 const PasswordField = (props) => {
   const { form, disabled, name, label, placeholder } = props;
@@ -14,16 +14,30 @@ const PasswordField = (props) => {
       disabled={disabled}
       placeholder={placeholder}
       render={(props) => (
-        <TextInput
-          {...props}
-          onChangeText={(value) => {
-            props.onChange(value);
-          }}
-        />
+        <View style={styles.container}>
+          <Text style={styles.text}>{label}</Text>
+          <TextInput
+            {...props}
+            style={styles.input}
+            secureTextEntry={true}
+            onChangeText={(value) => {
+              props.onChange(value);
+            }}
+          />
+        </View>
       )}
-      secureTextEntry={true}
     />
   );
 };
 
+const styles = StyleSheet.create({
+  container: { marginHorizontal: 24, marginVertical: 12, fontSize: 14, fontWeight: '500' },
+  text: { marginBottom: 12, marginLeft: 6, color: '#FFF' },
+  input: {
+    padding: 16,
+    color: '#FFF',
+    backgroundColor: '#1F1F30',
+    borderRadius: 18,
+  },
+});
 export default PasswordField;
