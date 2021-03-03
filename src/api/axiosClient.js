f785b5b4dbcc3450a3416fe91e5696d5c790d3e0
malburo <tongquocbao0605@gmail.com>
2021-03-03 21:46:51 +0700

    setup axios interceptor

---
+++ src/api/axiosClient.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import queryString from 'query-string';
+
+const request = axios.create({
+  baseURL: 'http://192.168.1.5:8000/api',
+  headers: {
+    'content-type': 'application/json',
+  },
+  paramsSerializer: (params) => queryString.stringify(params),
+});
+
+request.interceptors.request.use(
+  (config) => {
+    const token = '123';
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error.response.data.error || error.message)
+);
+
+request.interceptors.response.use(
+  (response) => {
+    if (response && response.data) {
+      return response.data.data;
+    }
+    return response;
+  },
+  async (error) => {
+    // if (error.response.status === 403) {
+    //   localStorage.removeItem(StorageKeys.ACCESS_TOKEN);
+    //   window.location.replace('/auth/login');
+    // }
+    // if (error.response.status === 404) {
+    //   window.location.replace('/404');
+    // }
+    return Promise.reject(error.response.data.error || error.message);
+  }
+);
+export default request;
